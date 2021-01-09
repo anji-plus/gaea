974b8f3563a641f0f8ca0c1a0b4e2b7e095683d4
raodeming <1130305001@qq.com>
2021-01-09 15:21:44 +0800

    Create EasyExcelTest.java

---
+++ service/springcloud/auth-service/src/test/java/easyExcel/EasyExcelTest.java
@@ -0,0 +1,107 @@
+package easyExcel;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.fill.FillConfig;
+import com.anji.mirror.auth.AuthApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.util.*;
+
+/**
+ * Created by raodeming on 2020/11/30.
+ */
+@Slf4j
+//@SpringBootTest(classes = AuthApplication.class)
+//@RunWith(SpringRunner.class)
+public class EasyExcelTest {
+
+    @Test
+    public void test1(){
+
+        String fileName = EasyExcelTest.getPath() + "simple" + System.currentTimeMillis() + ".xls";
+        System.out.println(fileName);
+        EasyExcel.write("D:\\aaa.xls", DemoData.class).sheet().doWrite(data());
+    }
+
+
+
+
+    @Test
+    public void test2(){
+        String templateFileName =
+                EasyExcelTest.getPath() + File.separator + "fill" + File.separator + "complex.xls";
+        String fileName = EasyExcelTest.getPath() + "complexFill" + System.currentTimeMillis() + ".xls";
+        fileName = "D:\\1111111.xls";
+        System.out.println(fileName);
+        ExcelWriter excelWriter = EasyExcel.write(fileName).withTemplate(templateFileName).build();
+        WriteSheet writeSheet = EasyExcel.writerSheet().build();
+        // 这里注意 入参用了forceNewRow 代表在写入list的时候不管list下面有没有空行 都会创建一行，然后下面的数据往后移动。默认 是false，会直接使用下一行，如果没有则创建。
+        // forceNewRow 如果设置了true,有个缺点 就是他会把所有的数据都放到内存了，所以慎用
+        // 简单的说 如果你的模板有list,且list不是最后一行，下面还有数据需要填充 就必须设置 forceNewRow=true 但是这个就会把所有数据放到内存 会很耗内存
+        // 如果数据量大 list不是最后一行 参照下一个
+        FillConfig fillConfig = FillConfig.builder().forceNewRow(Boolean.TRUE).build();
+        excelWriter.fill(data(), fillConfig, writeSheet);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("date", "2019年10月9日13:28:28");
+        map.put("total", 1000);
+        excelWriter.fill(map, writeSheet);
+        excelWriter.finish();
+
+    }
+
+
+
+
+
+
+    public static String getPath() {
+        return EasyExcelTest.class.getResource("/").getPath();
+    }
+
+    private List<List<String>> head() {
+        List<List<String>> list = new ArrayList<List<String>>();
+        List<String> head0 = new ArrayList<String>();
+        head0.add("字符串" + System.currentTimeMillis());
+        List<String> head1 = new ArrayList<String>();
+        head1.add("数字" + System.currentTimeMillis());
+        List<String> head2 = new ArrayList<String>();
+        head2.add("日期" + System.currentTimeMillis());
+        list.add(head0);
+        list.add(head1);
+        list.add(head2);
+        return list;
+    }
+
+    private List<List<Object>> dataList() {
+        List<List<Object>> list = new ArrayList<List<Object>>();
+        for (int i = 0; i < 10; i++) {
+            List<Object> data = new ArrayList<Object>();
+            data.add("字符串" + i);
+            data.add(new Date());
+            data.add(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
+    private List<DemoData> data() {
+        List<DemoData> list = new ArrayList<DemoData>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56+i);
+            data.setUsername("用户" + i);
+            list.add(data);
+        }
+        return list;
+    }
+
+}
