7066c74e982a60a55649766c0ae0fceabd1dcb03
raodeming <1130305001@qq.com>
2021-01-09 15:18:23 +0800

    Create PushHistoryVO.java

---
+++ service/springcloud/push-service/src/main/java/com/anji/mirror/push/domain/vo/PushHistoryVO.java
@@ -0,0 +1,116 @@
+package com.anji.mirror.push.domain.vo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import java.time.LocalDateTime;
+import java.io.Serializable;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author anji gaea teams
+ * @since 2020-10-26
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class PushHistoryVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long pushId;
+
+    /**
+     * 模板类型，sms：短信；mail：邮件; dingtalk:
+     */
+    private String templateType;
+
+    /**
+     * 模板code
+     */
+    private String templateCode;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * sms:短信消息, mail:邮件标题, dingtalk: 钉钉消息
+     */
+    private String pushTitle;
+
+    /**
+     * 发送者
+     */
+    private String pushFrom;
+
+    /**
+     * 接受者,以,或;隔开
+     */
+    private String pushTo;
+
+    /**
+     * 发送的手机号（钉钉，短信）,以,隔开
+     */
+    private String mobiles;
+
+    /**
+     * 邮件抄送人,以,或;隔开
+     */
+    private String mailCopy;
+
+    /**
+     * 邮件密送人,以,或;隔开
+     */
+    private String mailBcc;
+
+    /**
+     * 操作者(短信-运营商、钉钉、邮箱)
+     */
+    private String operator;
+
+    /**
+     * 发送时间
+     */
+    private LocalDateTime sendTime;
+
+    /**
+     * 发送状态,1,成功;0,失败
+     */
+    private Integer sendStatus;
+
+    /**
+     * 发送结果
+     */
+    private String sendResult;
+
+    private String createdBy;
+
+    private LocalDateTime createdTime;
+
+    private String updatedBy;
+
+    private LocalDateTime updatedTime;
+
+    /************************** 以下为非原表字段 ****************************/
+    /**
+     * 模糊搜索 结束时间
+     */
+    private String endTime;
+    /**
+     * 模糊搜索 搜索关键词
+     */
+    private String keyword;
+    /**
+     * 模糊搜索 开始时间
+     */
+    private String startTime;
+
+}
