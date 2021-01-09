a9e43cb6797fbb634bd494e156d9b7f568e1c568
大时代的小青年呀 <497157812@qq.com>
2021-01-09 14:33:58 +0800

    Create PushServiceApplicationTests.java

---
+++ service/springcloud/push-service/src/test/java/com/anji/mirror/push/PushServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anji.mirror.push;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
