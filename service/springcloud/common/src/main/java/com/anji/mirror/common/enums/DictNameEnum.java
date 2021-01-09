c549bc4c83565a1b2b6a0c580310b87b41acc4ab
chengkening <chengkening@anji-plus.com>
2021-01-09 15:09:59 +0800

    Create DictNameEnum.java

---
+++ service/springcloud/common/src/main/java/com/anji/mirror/common/enums/DictNameEnum.java
@@ -0,0 +1,12 @@
+package com.anji.mirror.common.enums;
+
+/**
+ * @author anji gaea teams
+ * @since 2020/10/23 16:32
+ * 数据字典code。 t_dict.dict_name
+ */
+public enum DictNameEnum {
+
+    DEVICE_TYPE; // 设备类型
+
+}
