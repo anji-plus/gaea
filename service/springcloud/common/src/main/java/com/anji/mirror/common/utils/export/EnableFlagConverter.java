834f35d27ce578bc8339a2f90f0ac2fe20d81777
chengkening <chengkening@anji-plus.com>
2021-01-09 15:09:29 +0800

    Create EnableFlagConverter.java

---
+++ service/springcloud/common/src/main/java/com/anji/mirror/common/utils/export/EnableFlagConverter.java
@@ -0,0 +1,71 @@
+package com.anji.mirror.common.utils.export;
+
+
+import com.alibaba.excel.converters.Converter;
+import com.alibaba.excel.enums.CellDataTypeEnum;
+import com.alibaba.excel.metadata.CellData;
+import com.alibaba.excel.metadata.GlobalConfiguration;
+import com.alibaba.excel.metadata.property.ExcelContentProperty;
+import com.anji.mirror.common.enums.EnableFlagEnum;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by raodeming on 2020/11/27.
+ */
+public class EnableFlagConverter implements Converter<Integer> {
+
+
+    /**
+     * Back to object types in Java
+     *
+     * @return Support for Java class
+     */
+    @Override
+    public Class supportJavaTypeKey() {
+        return Integer.class;
+    }
+
+    /**
+     * Back to object enum in excel
+     *
+     * @return Support for {@link CellDataTypeEnum}
+     */
+    @Override
+    public CellDataTypeEnum supportExcelTypeKey() {
+        return CellDataTypeEnum.STRING;
+    }
+
+    /**
+     * Convert excel objects to Java objects
+     *
+     * @param cellData            Excel cell data.NotNull.
+     * @param contentProperty     Content property.Nullable.
+     * @param globalConfiguration Global configuration.NotNull.
+     * @return Data to put into a Java object
+     * @throws Exception Exception.
+     */
+    @Override
+    public Integer convertToJavaData(CellData cellData, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) throws Exception {
+        return null;
+    }
+
+    /**
+     * Convert Java objects to excel objects
+     *
+     * @param value               Java Data.NotNull.
+     * @param contentProperty     Content property.Nullable.
+     * @param globalConfiguration Global configuration.NotNull.
+     * @return Data to put into a Excel
+     * @throws Exception Exception.
+     */
+    @Override
+    public CellData<String> convertToExcelData(Integer value, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) throws Exception {
+        if (value.equals(EnableFlagEnum.DISABLE.getCodeValue())) {
+            return new CellData<>(EnableFlagEnum.DISABLE.getCodeDesc());
+        }
+        return new CellData<>(EnableFlagEnum.ENABLE.getCodeDesc());
+    }
+}
+
