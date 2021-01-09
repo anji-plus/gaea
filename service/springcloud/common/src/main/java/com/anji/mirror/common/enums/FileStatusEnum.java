322eecb399c965aeeda6a6a6596659144f4ca6a9
renbaolong <renbaolong@anji-plus.com>
2021-01-09 15:12:18 +0800

    Create FileStatusEnum.java

---
+++ service/springcloud/common/src/main/java/com/anji/mirror/common/enums/FileStatusEnum.java
@@ -0,0 +1,48 @@
+package com.anji.mirror.common.enums;
+public enum FileStatusEnum {
+    CREATING("creating","生成中"),
+    SUCCESS("success","生成成功"),
+    FAILED("failed","生成失败"),
+    ;
+
+    private String codeValue;
+    private String codeDesc;
+
+    private FileStatusEnum(String  codeValue, String codeDesc) {
+        this.codeValue = codeValue;
+        this.codeDesc = codeDesc;
+    }
+
+    public String   getCodeValue(){ return this.codeValue;}
+
+    public String getCodeDesc(){ return this.codeDesc;}
+
+    //根据codeValue获取枚举
+    public static FileStatusEnum parseFromCodeValue(String codeValue){
+        for (FileStatusEnum e : FileStatusEnum.values()){
+            if(e.codeValue.equals(codeValue)){ return e;}
+        }
+        return null;
+    }
+
+    //根据codeValue获取描述
+    public static String getCodeDescByCodeBalue(String codeValue){
+        FileStatusEnum enumItem = parseFromCodeValue(codeValue);
+        return enumItem == null ? "" : enumItem.getCodeDesc();
+    }
+
+    //验证codeValue是否有效
+    public static boolean validateCodeValue(String codeValue){ return parseFromCodeValue(codeValue)!=null;}
+
+    //列出所有值字符串
+    public static String getString(){
+        StringBuffer buffer = new StringBuffer();
+        for (FileStatusEnum e : FileStatusEnum.values()){
+            buffer.append(e.codeValue).append("--").append(e.getCodeDesc()).append(", ");
+        }
+        buffer.deleteCharAt(buffer.lastIndexOf(","));
+        return buffer.toString().trim();
+    }
+
+
+}
