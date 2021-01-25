df8bb7e5301de03ede9ff5b4fe4aa9f3b8c35405
zhangyukuan <zhangyukuan@anji-plus.com>
2021-01-25 15:35:24 +0800

    删除菜单列表中的401和404页面

--- web/src/router/modules/webList.js
+++ web/src/router/modules/webList.js
@@ -19,32 +19,4 @@ export const webList = [
       },
     ],
   },
-  {
-    path: '/error',
-    redirect: '/error/404',
-    meta: {
-      title: 'error',
-      icon: 'error',
-    },
-    children: [
-      {
-        path: '401',
-        component: 'error-page/401',
-        name: 'Error401',
-        meta: {
-          title: '401',
-          icon: '',
-        },
-      },
-      {
-        path: '404',
-        component: 'error-page/404',
-        name: 'Error404',
-        meta: {
-          title: '404',
-          icon: '',
-        },
-      },
-    ],
-  },
 ]
