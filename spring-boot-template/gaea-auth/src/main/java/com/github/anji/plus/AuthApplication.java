28c74c9402224c89b91184e0fd1dd8ccbef005a8
lirui <lirui@anji-plus.com>
2021-01-29 17:26:12 +0800

    提交部分类

---
+++ spring-boot-template/gaea-auth/src/main/java/com/github/anji/plus/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.github.anji.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * auth模块
+ * @author lirui
+ * @since 2020-01-28
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(AuthApplication.class);
+    }
+}
---
+++ spring-boot-template/gaea-business/src/main/java/com/github/anji/plus/App.java
@@ -0,0 +1,13 @@
+package com.github.anji.plus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
