65b2f3d407da5011b78c3b6335768b54259d5efc
zhangyukuan <zhangyukuan@anji-plus.com>
2021-02-08 17:04:00 +0800

    替换只有一个子菜单的树

--- web-template/src/views/authority/role/components/RoleMenu.vue
+++ web-template/src/views/authority/role/components/RoleMenu.vue
@@ -71,35 +71,21 @@ export default {
     async getAllMenus() {
       const { code, data } = await allMenu()
       if (code != '200') return
-      // const hasChildren = data
+      const hasChildren = data
+      // 将一级菜单就是页面的树过滤出来，直接展示
+      hasChildren.forEach((item, index) => {
+        if (item.children.length == 1) {
+          hasChildren.splice(index, 1, item.children)
+        }
+      })
       // data.forEach()
-      this.menuData = data
+      this.menuData = hasChildren
     },
     async getCheckedId() {
       const { code, data } = await actionTree(this.id)
       if (code != '200') return
       this.checkedKeys = data
     },
-    // // 获取非叶子节点的菜单id
-    // getParentMenuIds(list) {
-    //   list.forEach((item) => {
-    //     if (item.children && item.children.length != 0) {
-    //       this.parentMenuIds.push(item.id)
-    //       this.getParentMenuIds(item.children)
-    //     }
-    //   })
-    // },
-    //  只留下叶子节点的id（去除父节点的id）
-    // menuIds是包含父节点的已选中id的list
-    // deleteParentMenuIds(menuIds) {
-    //   this.checkedKeys = menuIds
-    // },
-    // 获取选中菜单的id
-    getMenuIds() {
-      // return this.$refs.roleTree.getCheckedKeys().concat(...this.$refs.roleTree.getHalfCheckedKeys())
-      return this.$refs.roleTree.getCheckedKeys()
-    },
-
     // 弹窗关闭之前需要执行的逻辑
     closeDialog() {
       this.$emit('handleClose')
@@ -107,10 +93,3 @@ export default {
   },
 }
 </script>
-<style lang="scss">
-.tree_dialog .el-dialog__body {
-  height: 60vh;
-  overflow: hidden;
-  overflow-y: auto;
-}
-</style>
