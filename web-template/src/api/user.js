eba6c24149eb502466f4532bd81a7d59848113a2
zhangyukuan <zhangyukuan@anji-plus.com>
2021-03-01 17:31:31 +0800

    去掉获取菜单信息的公共提示

--- web-template/src/api/user.js
+++ web-template/src/api/user.js
@@ -2,7 +2,7 @@
  * @Author: zyk
  * @Date: 2020-07-13 15:13:37
  * @Last Modified by: zyk
- * @Last Modified time: 2021-02-07 16:18:16
+ * @Last Modified time: 2021-03-01 10:22:59
  */
 import request from '@/utils/request'
 // 登录、登出、获取菜单信息模块 独立存在于其他模块的请求之外
@@ -11,16 +11,7 @@ export function login(data) {
   return request({
     url: '/auth/login',
     method: 'post',
-    // headers: {
-    //   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    // },
     data,
-    // transformRequest: [
-    //   function(data) {
-    //     data = Qs.stringify(data)
-    //     return data
-    //   }
-    // ]
   })
 }
 
@@ -28,6 +19,7 @@ export function getInfo(orgCode) {
   return request({
     url: `/auth/menu/menuUserInfoByOrg`,
     // url: `/auth/menu/userInfo`,
+    headers: { noPrompt: true }, // 不要提示
     method: 'POST',
     data: { orgCode: '' },
   })
@@ -35,7 +27,7 @@ export function getInfo(orgCode) {
 
 // export function logout() {
 //   return request({
-//     url: '/vue-element-admin/user/logout',
+//     url: '',
 //     method: 'post'
 //   })
 // }
